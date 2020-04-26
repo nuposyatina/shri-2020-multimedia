23a6e72492355ef46d99d23fd400eb30ffa5dec1
Anna Tsukanova <nuposyatina@gmail.com>
2020-04-26 21:09:30 +0300

    Доделала разворачивание/сворачивание видео

--- resize.js
+++ resize.js
@@ -1,4 +1,17 @@
+let selectedVideo = null;
 const onClickVideo = (e) => {
   const videoElement = e.target;
+  const settingsContainer = document.querySelector('.settings');
+  selectedVideo = videoElement;
   videoElement.classList.add('full-screen');
+  settingsContainer.classList.add('visible');
+}
+
+const onCloseVideo = (e) => {
+  if (!selectedVideo) return;
+  selectedVideo.classList.remove('full-screen');
+  const settingsContainer = document.querySelector('.settings');
+  settingsContainer.classList.remove('visible');
+  selectedVideo = null;
+
 }
